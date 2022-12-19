60f4f827a01a1d9245c3936998c5725f696341f2
aya borhamm <114094637+aya20044@users.noreply.github.com>
2022-12-19 21:03:09 +0200

    Add files via upload

---
+++ Project 2.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Perfectnumbersapplication
+{
+    class App2
+    {
+        static void Main(string[] args)
+        { /* project 2 */
+            // start
+
+            Console.WriteLine(" Please, Enter Your Range Number from n1 to n2");
+            Console.WriteLine(" Enter n1 ");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter n2");
+            double y = double.Parse(Console.ReadLine());
+            Console.WriteLine(" These are the peerfect numbers between your range: ");
+            double sum = 0;
+            for ( double a=x; a<=y;a++)
+            { if (a == 0) continue;
+                for(double k = 1; k < a; k++)
+                {
+                    if (a % k == 0)
+                    { sum = sum + k; }
+                    else continue;
+                }
+                if (sum == a) { Console.Write(a + "\t"); sum = 0; }
+                if (sum != a) sum = 0;
+            }
+            Console.ReadKey();
+            // end 
+           
+          
+
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ project number one .cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+// converion using casting operation
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System;
+namespace Projectone
+{
+    class project1
+    {
+        static void Main(string[] args)
+        { // project one discreet task // prime numbers //start
+
+            double i = 0, j = 0;
+            Console.WriteLine("enter the first number,please");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter the second number,please");
+            double num2 = double.Parse(Console.ReadLine());
+            Console.Write("\n");
+            if (num1+num2<0 && num2<num1) { Console.Write(" invalid operation prime numbers are greater than 1"); }
+            for (i = num1; i <= num2; i++)
+            { 
+                if (i + num2<0) { Console.Write(" invalid operation prime numbers are greater than 1"); break; }
+                if (i == 0 || i == 1)
+                    continue;
+                for (j = 2; j < i; j++)
+                {
+                    if (i % j == 0) break;
+                    if (i % j != 0) ;
+
+                }
+                if (i == j) Console.WriteLine(i);
+            } 
+            if (i > 2) Console.WriteLine("The list of your prime numbers.");
+            Console.ReadLine();
+        }
+    }
+}
+//End 
